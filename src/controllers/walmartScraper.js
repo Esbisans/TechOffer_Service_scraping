0748dd3f2a185251c0386125e4fb7caa747f4dc4
Esau Sanchez <tonalfan@gmail.com>
2022-12-11 10:48:33 -0600

    update liverpool

--- src/controllers/walmartScraper.js
+++ src/controllers/walmartScraper.js
@@ -1,5 +1,5 @@
 const scraperWalmart = {
-    url: 'https://www.walmart.com.mx/',
+    url: 'https://www.liverpool.com.mx/tienda/home',
     async scraper(browser, searchString){
         const page = await browser.newPage();
         await page.setDefaultTimeout(60000);
@@ -7,28 +7,30 @@ const scraperWalmart = {
 
 
         try {
-            await page.goto(this.url);
+            page.setUserAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36')
+            await page.setViewport({ width: 1366, height: 768});
+            await page.goto(this.url, {waitUntil: 'networkidle2', timeout: 0});
             console.log(`wait for searchbarinput`);
-            //await page.waitForSelector('input[data-automation-id="search-bar"]');
-            await page.waitForNavigation();
-            await page.type('input[data-automation-id="search-bar"]', searchString);
+            //await page.waitForSelector('#mainSearchbar"]');
+            //await page.waitForNavigation();
+            await page.type('#mainSearchbar', searchString);
             await page.keyboard.press('Enter');
             console.log(`Searching to ${searchString}...`);
             //await page.waitForNavigation({waitUntil: 'networkidle2', timeout: 0});
-            await page.waitForSelector('.product_productCardSummary___YXeD > a');
+            await page.waitForSelector('.m-product__card > a');
 
-            await page.click('.product_productCardSummary___YXeD > a');
-            console.log(`navigating to .product_productCardSummary___YXeD`);
-            await page.waitForSelector('.product-details_productDetails__Uunoq');
-            //await page.waitForNavigation();
+            await page.click('.m-product__card > a');
+            console.log(`navigating to .m-product__card`);
+            //await page.waitForSelector('.product-details_productDetails__Uunoq');
+            await page.waitForNavigation();
             
             let dataObj = {};
             
-            dataObj['store'] = 'Walmart';
+            dataObj['store'] = 'Liverpool';
             dataObj['url'] = await page.$eval('link[rel="canonical"]', link => link.href);
-            dataObj['name'] = await page.$eval('h1[data-automation-id="product-details-header"]', text => text.textContent);
-            dataObj['price'] = await page.$eval('h4[data-automation-id="product-price"]', text => text.textContent);
-            dataObj['image'] = await page.$eval('.image-picker_mainImageContainer__g8rrD > img', img => img.src);
+            dataObj['name'] = await page.$eval('h1.a-product__information--title', text => text.textContent);
+            dataObj['price'] = (await page.$eval('p.a-product__paragraphDiscountPrice', text => text.textContent));
+            dataObj['image'] = await page.$eval('.m-img-pdp', img => img.src);
 
 
             await page.close();
