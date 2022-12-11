ad65ccf7a7768f8b3d695849858709cb02314abe
Esau Sanchez <tonalfan@gmail.com>
2022-12-10 18:26:59 -0600

    update ec2

--- src/routes/index-router.js
+++ src/routes/index-router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/" , (req, res) => {
-    res.send('conecction to app scraping v1.3');
+    res.send('conecction to app scraping v2.3');
 })
 
 module.exports = router
